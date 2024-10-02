b0cf905f93572e706d8d92c5173426806bb1d437
EstebanPorras <esteban.porrasmarin@ucr.ac.cr>
2024-10-01 19:34:06 -0600

    Quinto commit

---
+++ rk4/rk4.py
@@ -0,0 +1,25 @@
+def rk4(func, oper, state, h):
+   
+    """Calculo de RK4.
+
+    Examples:
+        >>> rk4(a,b,c,d)
+        7.0
+
+    Args:
+        func (función): First argument
+        oper (numpy.ndarray): Second argument
+        state (numpy.ndarray): Third argument
+        h (numpy.float64): Fourth argument 
+
+    Returns:
+        numpy.ndarray: Returns the sum of the `state`  
+
+    """
+    k1 = h * func(oper, state)
+    k2 = h * func(oper, state + 0.5 * k1)
+    k3 = h * func(oper, state + 0.5 * k2)
+    k4 = h * func(oper, state + k3)
+
+    return state + (1/6) * (k1 + 2*k2 + 2*k3 + k4)
+
